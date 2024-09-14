69c36f43b956b67b10d857f961447528a94107fa
SriramSameer <sriramsameer22@gmail.com>
2024-09-14 14:09:08 +0530

    Add the files

---
+++ src/sept/ex_06092024/Lab008.java
@@ -0,0 +1,16 @@
+// Below code is package : folder with sept -> ex_o6 folder
+package sept.ex_06092024;
+/* Author : Pramod
+*
+* Write anything here
+*
+* */
+
+// Program Java with the class file, name of the class is Lab008
+public class Lab008 {
+        // This is main function, JVM starts from here
+    public static void main(String[] args) {
+        //Below code will be executed, it will print hello world
+        System.out.println("Hello World!");
+    }
+}
