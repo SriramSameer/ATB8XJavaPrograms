95c028460be05d99bc74d0310c77e58e126774df
SriramSameer <sriramsameer22@gmail.com>
2024-09-15 12:57:44 +0530

    All the files - 11 Sept

---
+++ src/sept/ex_11092024_DataTypes/Lab015.java
@@ -0,0 +1,25 @@
+package sept.ex_11092024_DataTypes;
+
+public class Lab015 {
+    public static void main(String[] args) {
+        // Primitive Data Types
+        byte b = 100; // 8 bits, 1 Byte
+        short s = 128; // 16 bits, 2 Bytes
+        int i = 1000; // 32 bits, 4 Byte
+        long phone_number = 7337412331l; // 64 bits, 8 Bytes
+        long phone_number2 = 9963055977l; //64 bits, 8 Bytes
+
+        char grade = 'A'; // 'A', b, c, e, d, el -> ASCII value - int - assogned
+        // 16 buts, 2 Byte
+
+        float f = 3.14f;
+        float f2 = 3.14F; // 32 bits, 4 Byte
+
+        double d = 118.987986 //64 bits, 8 Byte
+
+
+        // Non primitive data Types (user defined)
+        String name = "Pramod"; // String - bunch of char (collection of chars)
+        // Array - Class - User Data Types
+    }
+}
---
+++ src/sept/ex_11092024_DataTypes/Lab016.java
@@ -0,0 +1,15 @@
+package sept.ex_11092024_DataTypes;
+
+public class Lab016 {
+    public static void main(String[] args) {
+        // Store the distance between earth and moon
+        long distance_between_earth_moon = 38440000l;
+        double distance_between_earth_moon2 = 38440000.865;
+
+        // max age
+        byte age = 122;
+        int age1 = 100; // wasting memory - 214768348488
+
+        // int, double - 99%
+    }
+}
