7b95080f63180e92627ea5e1252b1ad6e0c72d50
SriramSameer <sriramsameer22@gmail.com>
2024-09-19 12:00:52 +0530

    Assignment - 13 Sept

---
+++ src/sept/ex_13092924/Assign2.java
@@ -0,0 +1,19 @@
+package sept.ex_13092924;
+
+public class Assign2 {
+    public static void main(String[] args) {
+        double a = 34;
+        double b = 10;
+        double addition = a+b;
+        double subtraction = a-b;
+        double multiplication = a*b;
+        double division = a/b;
+        double remainder = a%b;
+
+        System.out.println("Addition of a and b is "+addition);
+        System.out.println("Subtarction of a and b is "+subtraction);
+        System.out.println("Multiplication of a and b is "+multiplication);
+        System.out.println("Division of a and b is "+division);
+        System.out.println("Remainder of a and b is "+remainder);
+    }
+}
