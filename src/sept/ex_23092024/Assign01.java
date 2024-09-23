88ff1a4aef8307f0d363e12867274c414a8b011a
SriramSameer <sriramsameer22@gmail.com>
2024-09-23 18:15:57 +0530

    All Files - 23 Sept

---
+++ src/sept/ex_23092024/Assign01.java
@@ -0,0 +1,25 @@
+package sept.ex_23092024;
+
+public class Assign01 {
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 2;
+        char ch = '+';
+        switch (ch) {
+            case '+':
+                System.out.println(num1 + num2);
+                break;
+            case '-':
+                System.out.println(num1 - num2);
+                break;
+            case '*':
+                System.out.println(num1 * num2);
+                break;
+            case '/':
+                System.out.println(num1 / num2);
+                break;
+            default:
+                System.out.println("Operation not mentioned");
+        }
+    }
+}
---
+++ src/sept/ex_23092024/Lab089.java
@@ -0,0 +1,11 @@
+package sept.ex_23092024;
+
+public class Lab089 {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
