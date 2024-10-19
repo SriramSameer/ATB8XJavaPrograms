dd92143e98f9e6a925e8791490f338bab2e6db0d
SriramSameer <sriramsameer22@gmail.com>
2024-10-19 13:14:20 +0530

    All files - 14 Oct

---
+++ src/oct/ex_14102024/BaseClass.java
@@ -0,0 +1,9 @@
+package oct.ex_14102024;
+
+public class BaseClass {
+    // Web Automation
+    // Single
+    // Hide the Functionality of the open and close Browser.
+    void openBrowser(String browser){}
+    void closeBrowser(String browser){}
+}
